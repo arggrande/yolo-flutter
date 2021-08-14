b230e9c5da68a2ba0299aa102eeb2c1a7620aab9
Adam Grande <adam.grande@outlook.com>
2021-08-14 16:43:02 +0800

    Added rough cut of mac/linux setupo

---
+++ linux-setup.sh
@@ -0,0 +1,14 @@
+echo "Installing Git..."
+brew install git
+
+echo "Installing VSCode..."
+brew install --cask visual-studio-code
+
+echo "Installing Android Studio..."
+brew install --cask android-studio
+
+echo "Installing flutter..."
+brew install --cask flutter
+
+echo "Accepting all android licenses..."
+flutter doctor --android-licenses
